8e4d6dd13a5d12f26d7f2c8ffdeaefd5e89c2fd4
ArinDev <arindev30@gmail.com>
2024-08-30 19:01:04 +0530

    Added vercel.config to backend

--- frontend/app/create-game/page.tsx
+++ frontend/app/create-game/page.tsx
@@ -11,7 +11,9 @@ export default function BingoTable() {
   const [size, setSize] = useState(3);
   const [table, setTable] = useState(Array(size).fill(null).map(() => Array(size).fill(null)));
   const [filled, setFilled] = useState(0);
-  // const [filled, setFilled] = useState(0);
+  const [prize, setPrize] = useState('');
+  const [gameName, setGameName] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const { token } = useAuthToken();
   const [loading, setLoading] = useState(true);
@@ -24,62 +26,81 @@ export default function BingoTable() {
     }
   }, [token, loading]);
 
+  useEffect(()=>{
+    setErrorMessage('');
+  }, [size, table, filled, gameName])
 
   if(!token)
     return ;
-  
+
   const handleSizeChange = (e : any) => {
     const newSize = parseInt(e.target.value)
     setSize(newSize);
+    setFilled(0);
     setTable(Array(newSize).fill(null).map(() => Array(newSize).fill(null)));
   };
 
   return (
     <div className="flex flex-col h-screen">
     <Header currentPage='Game' />
-    <div className="p-4 flex flex-col justify-center items-center bg-teal-100 justify-center items-center h-full">
-        <div className="b">BingoSize : <input
-            className="border p-2 mb-5"
-            type="number"
-            value={size}
-            onChange={handleSizeChange}
-            placeholder="Enter size (n)"
-            min="2"
-            max="5"
+    <div className="p-4 flex flex-col justify-center items-center bg-teal-100 h-full">
+        <div className="mb-5">
+            <input
+                className="border p-2 mb-2"
+                type="text"
+                placeholder="Name of the Game"
+                onChange={(e) => setGameName(e.target.value)}
             />
-          &nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; Filled : {filled}
+            <input
+                className="border p-2 mb-2"
+                type="number"
+                value={size}
+                onChange={handleSizeChange}
+                placeholder="Enter size (n)"
+                min="2"
+                max="5"
+            />
+            <input
+                className="border p-2 mb-5"
+                type="text"
+                placeholder="Prize for the winner"
+                onChange={(e) => setPrize(e.target.value)}
+            />
+            &nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; Filled : {filled}
         </div>
+        {errorMessage && <div className='text-red-500 text-center mb-6'>{errorMessage}</div>}
         <div className="grid border-black border-2" style={{ gridTemplateColumns: `repeat(${size}, 1fr)` }}>
             {table.map((row, rowIndex) =>
-            row.map((cell, colIndex) => (
-                <textarea
-                    key={`${rowIndex}-${colIndex}`}
-                    className={`border p-4 border-black border-1 ${cell ? 'bg-green-500' : 'bg-yellow-500'} text-white placeholder-white resize-none text-center`}
-                    value={cell || ""}
-                    placeholder={`Type here... ${rowIndex}-${colIndex}`}
-                    onChange={(e) => {
-                      const newValue = e.target.value;
-                      const oldValue = table[rowIndex][colIndex];
-                      const newTable = [...table]
-                      newTable[rowIndex][colIndex] = newValue;
-                      setTable(newTable);
-                      console.log("old : ", oldValue, " new : " ,newValue, table);
-                      if( (!oldValue && newValue))
-                          setFilled(filled => filled+1);
-                      else if( (oldValue && !newValue))
-                        setFilled(filled => filled-1);
-                    }}
-                    style={{ width: '100%', height: '100%', boxSizing: 'border-box' }}
+                row.map((cell, colIndex) => (
+                    <textarea
+                        key={`${rowIndex}-${colIndex}`}
+                        className={`border p-4 border-black ${cell ? 'bg-green-500' : 'bg-yellow-500'} text-white placeholder-white resize-none text-center`}
+                        value={cell || ""}
+                        placeholder={`Type here... ${rowIndex}-${colIndex}`}
+                        onChange={(e) => {
+                          const oldValue = table[rowIndex][colIndex];
+                          const newValue = !oldValue ? e.target.value.trim() : e.target.value;
+                          const newTable = [...table];
+                          newTable[rowIndex][colIndex] = newValue;
+                          setTable(newTable);
+                          if ((!oldValue && newValue)) setFilled(filled => filled + 1);
+                          else if ((oldValue && !newValue)) setFilled(filled => filled - 1);
+                          setErrorMessage('');
+                        }}
+                        style={{ width: '100%', height: '100%', boxSizing: 'border-box' }}
                     />
-                  ))
-                )
-              }
+                ))
+            )}
         </div>
         <div className='pt-4'>
-        <SubmitButton
-          onClick = { e => filled != size*size ? alert('Please fill all the entries') : alert('Bharne ke liye dhanyawad') }
-          label = 'Create Game!'
-          />
+            <SubmitButton
+                onClick={e => {
+                  if (filled !== size * size) setErrorMessage(`Fill the remaining ${size * size - filled} blocks`);
+                  !gameName && setErrorMessage(e => e ? e + " and give a name to the game" : "Give a name to the game")
+                  // filled !== size * size ? alert('Please fill all the entries') : alert(`Game Created: ${gameName}, Prize: ${prize}`)
+                }}
+                label='Create Game!'
+            />
         </div>
     </div>
     </div>
