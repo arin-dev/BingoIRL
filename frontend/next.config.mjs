74228773bc6b76dae960231f1e2ca9067383c2bd
ArinDev <arindev30@gmail.com>
2024-08-31 15:58:49 +0530

    next-config for separately hosted backend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,23 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    async rewrites() {
-      if (process.env.NODE_ENV === 'production') {
-        return [
-          {
-            source: '/api/:path*',
-            destination: `${process.env.NEXT_PUBLIC_PRODUCTION_DOMAIN}/api/:path*`,
-          },
-        ];
-      } else {
-        return [
-          {
-            source: '/api/:path*',
-            destination: `${process.env.NEXT_PUBLIC_DEV_DOMAIN}/api/:path*`,
-          },
-        ];
-      }
-    },
-  }
-  
-  export default nextConfig;
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_PRODUCTION_DOMAIN}/api/:path*`, // Points to your external API
+      },
+    ];
+  },
+};
+
+export default nextConfig;
